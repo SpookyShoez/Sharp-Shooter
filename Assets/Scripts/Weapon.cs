1e05acc5c6c981611f92f9475c0e20b00be4e32e
Morialle Barr <Moriallekbarr@gmail.com>
2025-09-01 18:09:12 +0300

    Lesson 6.18 (shoot cooldown)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,43 +1,19 @@
-using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
-    [SerializeField] int damageAmount = 1;
-
-    StarterAssetsInputs starterAssetsInputs;
-
-    const string SHOOT_STRING = "Shoot";
-
-    void Awake()
+    
+    public void Shoot(WeaponSO weaponSO)
     {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-
-    void Update()
-    {
-        HandleShoot();
-    }
-
-    void HandleShoot()
-    {
-        if (!starterAssetsInputs.shoot) return;
-
-        muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-        starterAssetsInputs.ShootInput(false);
-
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
-
+            muzzleFlash.Play();
+            Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
-            enemyHealth?.TakeDamage(damageAmount);
+            enemyHealth?.TakeDamage(weaponSO.Damage);
         }
     }
 }
---
+++ Assets/Scripts/WeaponSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(fileName = "WeaponSO", menuName = "ScriptableObjects/WeaponSO")]
+public class WeaponSO : ScriptableObject
+{
+    public int Damage = 1;
+    public float FireRate = .5f;
+    public GameObject HitVFXPrefab;
+
+}
